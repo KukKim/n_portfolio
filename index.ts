a17b8adb98a716814ec97510dcf8bf20345a67c0
Dongkuk Kim <dongkukkim@Dongkukui-MacBookPro.local>
2026-05-06 19:30:01 +0900

    feat: add signin, check

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 import mysql from "mysql2/promise";
 import express from "express";
 import bodyParser from "body-parser";
+import crypto from "crypto";
 
 // Create the connection to database
 const connection = await mysql.createConnection({
@@ -20,6 +21,126 @@ const urlencodedParser = bodyParser.urlencoded();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/check", jsonParser, async (req, res) => {
+  console.log("check - ");
+  const { token } = req.body;
+  console.log("token - ", token);
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      code: "TOKEN_REQUIRED",
+      message: "Token is required",
+    });
+  }
+  try {
+    const [users] = await connection.query(
+      "SELECT idx, name, email, imgUri, token, loginDate, expireDate FROM user WHERE token = ?",
+      [token],
+    );
+
+    if (users.length === 0) {
+      return res.status(401).json({
+        success: false,
+        code: "INVALID_TOKEN",
+        message: "Invalid token",
+      });
+    }
+
+    const user = users[0];
+    const now = new Date();
+    const expireDate = new Date(user.expireDate);
+
+    if (expireDate < now) {
+      await connection.query(
+        `
+        UPDATE user
+        SET token = NULL
+        WHERE idx = ?
+        `,
+        [user.idx],
+      );
+
+      return res.status(401).json({
+        success: false,
+        code: "TOKEN_EXPIRED",
+        message: "Token has expired. Please sign in again.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Token is valid",
+      data: {
+        // userId: user.idx,
+        // name: user.name,
+        // email: user.email,
+        // imgUri: user.imgUri,
+        // token: user.token,
+        // loginDate: user.loginDate,
+        // expireDate: user.expireDate,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      code: "SERVER_ERROR",
+      message: "Something went wrong",
+    });
+  }
+});
+
+// TODO: 후에 POST 로 변경 필요. POST에서 SSL로 변경
+app.get("/signin", jsonParser, async (req, res) => {
+  const { email, password } = req.query;
+  try {
+    const [users] = await connection.query(
+      "SELECT * FROM user WHERE email = '" +
+        email +
+        "' AND password = '" +
+        password +
+        "';",
+    );
+
+    if (users.length === 0) {
+      return res.status(401).json({
+        success: false,
+        code: "INVALID_CREDENTIALS",
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = users[0];
+    const token = crypto.randomBytes(32).toString("hex");
+    const loginDate = new Date();
+    const expireDate = new Date(loginDate);
+    expireDate.setDate(expireDate.getDate() + 100);
+
+    await connection.query(
+      "UPDATE user SET loginDate = ?, token = ?, expireDate = ? WHERE idx = ?",
+      [loginDate, token, expireDate, user.idx],
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Signin successful",
+      data: {
+        name: user.name,
+        imgUri: user.imgUri,
+        token,
+        loginDate,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      code: "SERVER_ERROR",
+      message: "Something went wrong",
+    });
+  }
 });
 
 app.post("/signup", jsonParser, async (req, res) => {
